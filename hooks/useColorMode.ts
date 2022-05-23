0c7523426b1aa348328ca06432b84db3d5e9506d
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-05-22 21:50:06 -0300

    Switch theme button remade

--- hooks/useColorMode.ts
+++ hooks/useColorMode.ts
@@ -3,8 +3,8 @@ import { useTheme } from "../contexts/ThemeContext"
 import logoDark from "../public/logo_dark.png"
 import logoLight from "../public/logo_light.png"
 
-import switchDark from "../public/switch_dark.svg"
-import switchLight from "../public/switch_light.svg"
+import switchDark from "../public/switch_dark_logos.svg"
+import switchLight from "../public/switch_light_logos.svg"
 
 export function useColorMode() {
   const { isDarkMode } = useTheme()
